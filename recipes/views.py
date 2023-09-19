754bb43f12bb92069cf3d05fa4809f6ed48db47c
VictoriaParkes <>
2023-09-19 16:44:49 +0000

    Add navbar structure

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Recipe
 
-# Create your views here.
+def home(request):
+
+    template = "index.html"
+    context = {
+        "page_title": "Home"
+    }
+
+    return render(request, template, context)
