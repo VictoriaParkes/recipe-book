10a2da93d436a9d33c39736d72dba28905539501
VictoriaParkes <>
2023-10-06 13:32:34 +0000

    Reverse recipes migrations, install django-jsonform, create recipe model

--- RECIPE_BOOK/settings.py
+++ RECIPE_BOOK/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'cloudinary',
     'crispy_forms',
+    'django_jsonform',
     'recipes',
 ]
 
--- recipes/models.py
+++ recipes/models.py
@@ -2,16 +2,17 @@ from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 from autoslug import AutoSlugField
+from django_jsonform.models.fields import JSONField
 
 # choices
-RECIPE_COURSE = (
-    ('breakfast', 'Breakfast'),
-    ('lunch', 'Lunch'),
-    ('dinner', 'Dinner'),
-    ('dessert', 'Dessert'),
-    ('supper', 'Supper'),
-    ('snack', 'Snack'),
-)
+# RECIPE_COURSE = (
+#     ('breakfast', 'Breakfast'),
+#     ('lunch', 'Lunch'),
+#     ('dinner', 'Dinner'),
+#     ('dessert', 'Dessert'),
+#     ('supper', 'Supper'),
+#     ('snack', 'Snack'),
+# )
 
 APPROVAL_STATUS = (
     ('0', 'Pending Approval'),
@@ -19,6 +20,56 @@ APPROVAL_STATUS = (
     ('2', 'Rejected'),
 )
 
+# JSONField schemas
+COURSE_SCHEMA = {
+    'type': 'array',
+    'title': 'Course',
+    'items': {
+            'type': 'string',
+            'choices': [
+                'Breakfast',
+                'Lunch',
+                'Dinner',
+                'Dessert',
+                'Supper',
+                'Snack'
+            ],
+        'helpText': 'Select all that apply',
+        'widget': 'multiselect'
+    }
+}
+
+INGREDIENTS_SCHEMA = {
+    'type': 'array',
+    'title': 'Ingredients',
+    'description': 'Add the ingredients and amount needed for your recipe',
+    'items': {
+            'type': 'dict',
+            'keys': {
+                'ingredient': {
+                    'type': 'string',
+                    'required': True
+                },
+                'amount': {
+                    'type': 'string',
+                    'required': True
+                }
+            }
+    },
+    'minItems': 1
+}
+
+METHOD_SCHEMA = {
+    'type': 'array',
+    'title': 'Method',
+    'description': 'Add steps for your recipe method',
+    'items': {
+            'type': 'string',
+            'widget': 'textarea',
+            'required': True
+    },
+    'minItems': 1
+}
 
 # recipe model
 class Recipe(models.Model):
@@ -28,40 +79,17 @@ class Recipe(models.Model):
         User, on_delete=models.CASCADE, related_name='recipes'
     )
     recipe_image = CloudinaryField('image', default='placeholder')
-    course = models.CharField(max_length=50, choices=RECIPE_COURSE)
+    course = JSONField(schema=COURSE_SCHEMA)
     description = models.TextField(blank=True)
-    ingredients = models.JSONField(null=False)
-    method = models.JSONField(null=False)
+    ingredients = JSONField(schema=INGREDIENTS_SCHEMA)
+    method = JSONField(schema=METHOD_SCHEMA)
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
     publish_request = models.BooleanField(default=False)
     publish_approved = models.CharField(max_length=50, choices=APPROVAL_STATUS, default=0)
-    likes = models.ManyToManyField(
-        User, related_name='recipe_like', blank=True)
 
     class Meta:
-        ordering = ["-created_on"]
+        ordering = ['-created_on']
 
     def __str__(self):
         return self.title
-
-    def number_of_likes(self):
-        return self.likes.count()
-
-
-# comment model
-class Comment(models.Model):
-    recipe = models.ForeignKey(
-        Recipe, on_delete=models.CASCADE, related_name='comments'
-    )
-    name = models.CharField(max_length=80)
-    email = models.EmailField()
-    body = models.TextField()
-    created_on = models.DateTimeField(auto_now_add=True)
-    approved = models.BooleanField(default=False)
-
-    class Meta:
-        ordering = ['created_on']
-
-    def __str__(self):
-        return f'Comment {self.body} by {self.name}'
