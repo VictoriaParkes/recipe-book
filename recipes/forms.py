d9e93945fa796773383f1fcc6645b706ee924566
VictoriaParkes <>
2023-10-28 14:21:00 +0000

    Add commenting feature to recipe details

--- recipes/forms.py
+++ recipes/forms.py
@@ -73,3 +73,9 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('body',)
+        widgets = {
+            'body': forms.Textarea(attrs={'rows': 3,}),
+        }
+        labels = {
+            'body': 'Add a comment'
+        }
