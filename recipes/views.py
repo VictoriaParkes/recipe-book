76f563e983b3bcd5837802bc6ea2a084460fa010
VictoriaParkes <>
2023-11-01 19:04:56 +0000

    Add page titles to error pages

--- recipes/views.py
+++ recipes/views.py
@@ -243,22 +243,34 @@ def handler400(request, exception):
     """
     Custom 400 page
     """
-    return render(request, "errors/400.html", status=400)
+    context = {
+        'page_title': '400'
+    }
+    return render(request, "errors/400.html", context, status=400)
 
 def handler403(request, exception):
     """
     Custom 403 page
     """
-    return render(request, "errors/403.html", status=403)
+    context = {
+        'page_title': '403'
+    }
+    return render(request, "errors/403.html", context, status=403)
 
 def handler404(request, exception):
     """
     Custom 404 page
     """
-    return render(request, "errors/404.html", status=404)
+    context = {
+        'page_title': '404'
+    }
+    return render(request, "errors/404.html", context, status=404)
 
 def handler500(request):
     """
     Custom 500 page
     """
-    return render(request, "errors/500.html", status=500)
+    context = {
+        'page_title': '500'
+    }
+    return render(request, "errors/500.html", context, status=500)
